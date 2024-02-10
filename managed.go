dc68c404e855d4ed5c3a74446cb945641a209ea3
Jorgen Nilsson <me@jorgennilsson.com>
2024-02-10 22:00:47 +0100

    Trying out a new things for transactions

--- managed.go
+++ managed.go
@@ -2,8 +2,15 @@ package cyphertxn
 
 import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
 )
 
+// ReadTX reads data from the database using the provided DatabaseService and Statement.
+// It returns an array of neo4j.Record pointers and an error if any.
+// The function creates a new session using the DatabaseService configuration,
+// and executes a read transaction within that session.
+// It collects the results of the executed transaction and returns them.
+// If any error occurs during the execution, an empty array and the error are returned.
 func ReadTX(ds *DatabaseService, statement Statement) ([]*neo4j.Record, error) {
 	session := ds.Driver.NewSession(ds.Ctx, neo4j.SessionConfig{
 		AccessMode:   neo4j.AccessModeRead,
@@ -27,7 +34,11 @@ func ReadTX(ds *DatabaseService, statement Statement) ([]*neo4j.Record, error) {
 	return res.([]*neo4j.Record), nil
 }
 
-func ManagedTx(ds *DatabaseService, work neo4j.ManagedTransactionWork, session neo4j.SessionWithContext) (any, error) {
+func ManagedTx(ds *DatabaseService, work neo4j.ManagedTransactionWork) (any, error) {
+	session := ds.Driver.NewSession(ds.Ctx, neo4j.SessionConfig{
+		DatabaseName: ds.Name,
+	})
+	defer session.Close(ds.Ctx)
 	result, err := session.ExecuteWrite(ds.Ctx, work)
 	if err != nil {
 		return nil, err
@@ -66,4 +77,18 @@ func Transactions(ds *DatabaseService, statements ...Statement) (any, error) {
 	}
 
 	return result, nil
+}
+
+func WorkUnit(ds *DatabaseService, s Statement, tx neo4j.ManagedTransaction) ([]*neo4j.Record, error) {
+	result, runErr := tx.Run(ds.Ctx, s.Query, s.Params)
+	if runErr != nil {
+		log.Fatalln(runErr)
+		return nil, runErr
+	}
+
+	records, err := result.Collect(ds.Ctx)
+	if err != nil {
+		return []*neo4j.Record{}, err
+	}
+	return records, nil
 }
