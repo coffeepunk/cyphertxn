bd82ff6ba41306217a22cc9cb1eb758e9123b7cf
Jörgen Nilsson <me@jorgennilsson.com>
2024-02-11 18:31:53 +0100

    Added context with cancel for simple query

--- simple.go
+++ simple.go
@@ -1,11 +1,14 @@
 package cyphertxn
 
 import (
+	"context"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func Query(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
-	return neo4j.ExecuteQuery(ds.Ctx, ds.Driver,
+	ctx, cancel := context.WithCancel(ds.Ctx)
+	defer cancel()
+	return neo4j.ExecuteQuery(ctx, ds.Driver,
 		statement.Query,
 		statement.Params, neo4j.EagerResultTransformer,
 		neo4j.ExecuteQueryWithDatabase(ds.Name))
