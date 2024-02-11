5ba9f418c4a1402b6873db8a1c3c319373822814
Jörgen Nilsson <me@jorgennilsson.com>
2024-02-11 19:09:32 +0100

    Incremental updates for handling read and write simple query (#2)
    * Added context with cancel for simple query
    
    * direct return for transactions
    
    * Added read and write routing to the simple query
    
    * Added two functions so that write and read are specifically used.

--- simple.go
+++ simple.go
@@ -1,12 +1,26 @@
 package cyphertxn
 
 import (
+	"context"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-func Query(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
-	return neo4j.ExecuteQuery(ds.Ctx, ds.Driver,
+func QueryWrite(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
+	ctx, cancel := context.WithCancel(ds.Ctx)
+	defer cancel()
+	return neo4j.ExecuteQuery(ctx, ds.Driver,
 		statement.Query,
 		statement.Params, neo4j.EagerResultTransformer,
-		neo4j.ExecuteQueryWithDatabase(ds.Name))
+		neo4j.ExecuteQueryWithDatabase(ds.Name),
+		neo4j.ExecuteQueryWithWritersRouting())
+}
+
+func QueryRead(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
+	ctx, cancel := context.WithCancel(ds.Ctx)
+	defer cancel()
+	return neo4j.ExecuteQuery(ctx, ds.Driver,
+		statement.Query,
+		statement.Params, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase(ds.Name),
+		neo4j.ExecuteQueryWithReadersRouting())
 }
