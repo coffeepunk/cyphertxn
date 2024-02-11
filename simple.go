688d14631366d531ddf8f0c7cb9ec7a70222b81a
Jörgen Nilsson <me@jorgennilsson.com>
2024-02-11 19:01:22 +0100

    Added read and write routing to the simple query
    Added two functions so that write and read are specifically used.

--- simple.go
+++ simple.go
@@ -5,11 +5,22 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-func Query(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
+func QueryWrite(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
 	ctx, cancel := context.WithCancel(ds.Ctx)
 	defer cancel()
 	return neo4j.ExecuteQuery(ctx, ds.Driver,
 		statement.Query,
 		statement.Params, neo4j.EagerResultTransformer,
-		neo4j.ExecuteQueryWithDatabase(ds.Name))
+		neo4j.ExecuteQueryWithDatabase(ds.Name),
+		neo4j.ExecuteQueryWithWritersRouting())
+}
+
+func QueryRead(ds DatabaseService, statement Statement) (*neo4j.EagerResult, error) {
+	ctx, cancel := context.WithCancel(ds.Ctx)
+	defer cancel()
+	return neo4j.ExecuteQuery(ctx, ds.Driver,
+		statement.Query,
+		statement.Params, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase(ds.Name),
+		neo4j.ExecuteQueryWithReadersRouting())
 }
